d17894c511262babf07267459771f5b81cb8f1f4
Elena Nazzi <elna@itu.dk>
2012-08-09 11:02:19 +0200

    adding oauth tools for retrieving oauth tokens for additional users in the future...

---
+++ OAuthTwitterExample/src/Main.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import oauth.signpost.OAuth;
+import oauth.signpost.OAuthConsumer;
+import oauth.signpost.OAuthProvider;
+import oauth.signpost.basic.DefaultOAuthConsumer;
+import oauth.signpost.basic.DefaultOAuthProvider;
+import oauth.signpost.signature.SignatureMethod;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        OAuthConsumer consumer = new DefaultOAuthConsumer(
+                "iIlNngv1KdV6XzNYkoLA",
+                "exQ94pBpLXFcyttvLoxU2nrktThrlsj580zjYzmoM",
+                SignatureMethod.HMAC_SHA1);
+
+        OAuthProvider provider = new DefaultOAuthProvider(consumer,
+                "http://twitter.com/oauth/request_token",
+                "http://twitter.com/oauth/access_token",
+                "http://twitter.com/oauth/authorize");
+
+        System.out.println("Fetching request token from Twitter...");
+
+        // we do not support callbacks, thus pass OOB
+        String authUrl = provider.retrieveRequestToken(OAuth.OUT_OF_BAND);
+
+        System.out.println("Request token: " + consumer.getToken());
+        System.out.println("Token secret: " + consumer.getTokenSecret());
+
+        System.out.println("Now visit:\n" + authUrl
+                + "\n... and grant this app authorization");
+        System.out.println("Enter the PIN code and hit ENTER when you're done:");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String pin = br.readLine();
+
+        System.out.println("Fetching access token from Twitter...");
+
+        provider.retrieveAccessToken(pin);
+
+        System.out.println("Access token: " + consumer.getToken());
+        System.out.println("Token secret: " + consumer.getTokenSecret());
+
+        URL url = new URL("http://twitter.com/statuses/mentions.xml");
+        HttpURLConnection request = (HttpURLConnection) url.openConnection();
+
+        consumer.sign(request);
+
+        System.out.println("Sending request to Twitter...");
+        request.connect();
+
+        System.out.println("Response: " + request.getResponseCode() + " "
+                + request.getResponseMessage());
+    }
+}
